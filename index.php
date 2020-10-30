28bc088415f7d93539385c0f87452f0104800222
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-10-30 17:54:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Hello World</title>
 </head>
 <body>
-    <h1>แนะนำตัว<h1></h1>
+    <h1>แนะนำตัว</h1>
     <p>นางสาววนัชพร ธรรมรัตนตันติ ชื่อเล่น หนุน</p>
     <img src="bkk1.jpg">
     <img src="bkk2.jpg">
@@ -19,7 +19,7 @@
         อาจารย์คะ! สถานที่ไหนในโรงเรียนมีปลาเยอะที่สุดคะ?<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>
         
         ตอบ หอปลาชุมค่ะ55555555555555
-    <p>
+    </p>
 
 </body>
 </html>
